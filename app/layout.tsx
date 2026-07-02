744f0defbf44a07f1419bdebef972d3ab372c42f
optical-lift <gleamingbrook@gmail.com>
2026-07-02 16:55:53 -0500

    Load task feed overrides

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import "./home.css";
 import "./zones.css";
+import "./task-feed.css";
 
 const geistSans = Geist({ variable: "--font-geist-sans", subsets: ["latin"] });
 const geistMono = Geist_Mono({ variable: "--font-geist-mono", subsets: ["latin"] });
