6bdcd0ed60325a39a6fccd6b4970d84136087b21
optical-lift <gleamingbrook@gmail.com>
2026-07-04 07:26:18 -0500

    Load closeout clean styles

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import "./globals.css";
 import "./home.css";
 import "./zones.css";
 import "./task-feed.css";
+import "./closeout-clean.css";
 
 const geistSans = Geist({ variable: "--font-geist-sans", subsets: ["latin"] });
 const geistMono = Geist_Mono({ variable: "--font-geist-mono", subsets: ["latin"] });
