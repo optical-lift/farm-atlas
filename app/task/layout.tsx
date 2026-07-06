0ec442db816503bc257339081ca94a2e0a7da3aa
optical-lift <gleamingbrook@gmail.com>
2026-07-06 16:55:52 -0500

    Load default task tools styles

--- app/task/layout.tsx
+++ app/task/layout.tsx
@@ -2,6 +2,7 @@ import "../task-action-temporary.css";
 import "../timing-language.css";
 import "../task-collection-focus.css";
 import "../task-progress-report.css";
+import "../default-task-tools.css";
 
 export default function TaskLayout(props: { children: React.ReactNode }) {
   return props.children;
