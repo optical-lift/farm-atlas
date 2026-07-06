ed44e110106a48a9415688583160121463baaa0c
optical-lift <gleamingbrook@gmail.com>
2026-07-05 23:11:14 -0500

    Import quick check capture styles

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,6 +10,7 @@ import "./log-picker-compact.css";
 import "./today-schedule.css";
 import "./project-spine.css";
 import "./task-page.css";
+import "./field-state-capture.css";
 
 const geistSans = Geist({ variable: "--font-geist-sans", subsets: ["latin"] });
 const geistMono = Geist_Mono({ variable: "--font-geist-mono", subsets: ["latin"] });
