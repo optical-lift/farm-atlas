21362db98916456fba6ec2300fdd62697107245e
optical-lift <gleamingbrook@gmail.com>
2026-07-06 12:04:04 -0500

    Load task collection focus styles

--- app/task/layout.tsx
+++ app/task/layout.tsx
@@ -1,5 +1,6 @@
 import "../task-action-temporary.css";
 import "../timing-language.css";
+import "../task-collection-focus.css";
 
 export default function TaskLayout(props: { children: React.ReactNode }) {
   return props.children;
