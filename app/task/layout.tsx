2ed1a037651eb1fee35c03be2880c2b0efd0a300
optical-lift <gleamingbrook@gmail.com>
2026-07-06 15:19:29 -0500

    Load task progress report styles

--- app/task/layout.tsx
+++ app/task/layout.tsx
@@ -1,6 +1,7 @@
 import "../task-action-temporary.css";
 import "../timing-language.css";
 import "../task-collection-focus.css";
+import "../task-progress-report.css";
 
 export default function TaskLayout(props: { children: React.ReactNode }) {
   return props.children;
