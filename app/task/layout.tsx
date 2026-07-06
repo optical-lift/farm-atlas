ff5343f62e4efc699e446567d52a49f7f81264da
optical-lift <gleamingbrook@gmail.com>
2026-07-06 17:16:19 -0500

    Load route date grouping styles

--- app/task/layout.tsx
+++ app/task/layout.tsx
@@ -3,6 +3,7 @@ import "../timing-language.css";
 import "../task-collection-focus.css";
 import "../task-progress-report.css";
 import "../default-task-tools.css";
+import "../route-date-groups.css";
 
 export default function TaskLayout(props: { children: React.ReactNode }) {
   return props.children;
