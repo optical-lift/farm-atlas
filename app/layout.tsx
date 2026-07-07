4ed3984384c75d242e30264a50724c21a44ab7c1
optical-lift <gleamingbrook@gmail.com>
2026-07-06 23:58:20 -0500

    Load exact-day task progress patch

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import WeekDayNavigation from "./WeekDayNavigation";
 import HomeTodayCompletePatch from "./HomeTodayCompletePatch";
+import TaskProgressExactDayPatch from "./TaskProgressExactDayPatch";
 import "./globals.css";
 import "./home.css";
 import "./zones.css";
@@ -36,6 +37,7 @@ export default function RootLayout({ children }: Readonly<{ children: React.Reac
       <body className="min-h-full flex flex-col">
         <WeekDayNavigation />
         <HomeTodayCompletePatch />
+        <TaskProgressExactDayPatch />
         {children}
       </body>
     </html>
