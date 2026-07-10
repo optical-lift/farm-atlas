088ed8af418a038355d283f16528a198aac450c3
optical-lift <gleamingbrook@gmail.com>
2026-07-10 14:24:45 -0500

    Load React-only child checklist guard last

--- app/task/layout.tsx
+++ app/task/layout.tsx
@@ -5,6 +5,8 @@ import "../task-progress-report.css";
 import "../default-task-tools.css";
 import "../route-date-groups.css";
 import "../route-today-header.css";
+import "../task-child-inline-log.css";
+import "../task-child-react-only.css";
 
 export default function TaskLayout(props: { children: React.ReactNode }) {
   return props.children;
