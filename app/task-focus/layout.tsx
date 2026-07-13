1a2441021274f2a160161dd6b228a16f53cc31eb
optical-lift <gleamingbrook@gmail.com>
2026-07-13 16:22:55 -0500

    Add task focus route layout

---
+++ app/task-focus/layout.tsx
@@ -0,0 +1,13 @@
+import "../task-action-temporary.css";
+import "../timing-language.css";
+import "../task-collection-focus.css";
+import "../task-progress-report.css";
+import "../default-task-tools.css";
+import "../route-date-groups.css";
+import "../route-today-header.css";
+import "../task-child-inline-log.css";
+import "../task-child-react-only.css";
+
+export default function TaskFocusLayout(props: { children: React.ReactNode }) {
+  return props.children;
+}
