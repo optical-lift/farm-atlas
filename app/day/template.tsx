3f4fe04fcf822757420601d061c8f7fd8480b9d2
optical-lift <gleamingbrook@gmail.com>
2026-07-14 17:11:26 -0500

    Remove day route remount shim blocking build

--- app/day/template.tsx
+++
@@ -1,9 +0,0 @@
-"use client";
-
-import { useSearchParams } from "next/navigation";
-import type { ReactNode } from "react";
-
-export default function DayRouteTemplate({ children }: { children: ReactNode }) {
-  const searchParams = useSearchParams();
-  return <div key={searchParams.toString()}>{children}</div>;
-}
