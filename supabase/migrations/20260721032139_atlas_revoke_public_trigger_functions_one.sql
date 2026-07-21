4ed905048077b91cf34af5e31306367be0083382
optical-lift <gleamingbrook@gmail.com>
2026-07-20 22:34:48 -0500

    Record public trigger function revokes one

---
+++ supabase/migrations/20260721032139_atlas_revoke_public_trigger_functions_one.sql
@@ -0,0 +1,6 @@
+revoke execute on function atlas.apply_sowing_projections() from public;
+revoke execute on function atlas.collapse_new_germination_duplicate_v1() from public;
+revoke execute on function atlas.create_delayed_followup_task() from public;
+revoke execute on function atlas.create_germination_check_after_sowing_done() from public;
+revoke execute on function atlas.enforce_no_sunday_task_due_date() from public;
+revoke execute on function atlas.enforce_weeding_21_day_cooldown() from public;
