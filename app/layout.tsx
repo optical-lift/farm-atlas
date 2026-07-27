6eeff1e206f4029b28572ab44c0e5cd0137bb0e2
optical-lift <gleamingbrook@gmail.com>
2026-07-27 15:29:53 -0500

    Load shared Atlas primitives globally

--- app/layout.tsx
+++ app/layout.tsx
@@ -14,6 +14,7 @@ import AnnaPaidScheduleHomePatch from "./AnnaPaidScheduleHomePatch";
 import TaskResultAnchorPatch from "./TaskResultAnchorPatch";
 import TendingTaskContext from "@/components/atlas/tending-task-context";
 import "./globals.css";
+import "./atlas-primitives.css";
 import "./home.css";
 import "./zones.css";
 import "./task-feed.css";
