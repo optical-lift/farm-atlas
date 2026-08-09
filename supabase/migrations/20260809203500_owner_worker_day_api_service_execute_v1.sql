7ba94d870750d78ff7a10bc158da943a96c6e224
optical-lift <gleamingbrook@gmail.com>
2026-08-09 15:55:32 -0500

    Grant service execution for canonical worker-day APIs

---
+++ supabase/migrations/20260809203500_owner_worker_day_api_service_execute_v1.sql
@@ -0,0 +1,2 @@
+grant execute on function atlas.owner_worker_day_plan_api_v1(uuid,uuid,date) to service_role;
+grant execute on function atlas.owner_build_worker_day_schedule_api_v2(uuid,uuid,date,jsonb) to service_role;
